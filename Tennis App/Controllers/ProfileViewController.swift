2e9a9acf7734a358975151d5914934ed4c071eb9
Adrian-Yao <adrian.c.yao@gmail.com>
2017-08-23 09:48:57 -0400

    Before the implementation of Autofill

--- Tennis App/Controllers/ProfileViewController.swift
+++ Tennis App/Controllers/ProfileViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import FirebaseAuth
 import FirebaseDatabase
 
+import SearchTextField
 class ProfileViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextViewDelegate {
     
     var age: String?
